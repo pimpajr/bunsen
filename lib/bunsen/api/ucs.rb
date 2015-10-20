d8759f9abe71e687052f996da47f4dab59558ca7
Patrick Pimentel Jr <existsec@gmail.com>
2015-10-20 13:40:13 -0700

    implemented ucs vlan configuration support from yaml

--- lib/bunsen/api/ucs.rb
+++ lib/bunsen/api/ucs.rb
@@ -2,6 +2,7 @@ require 'ucsimc'
 module Bunsen
   class UCS < Bunsen::API
     attr_accessor :connection
+    attr_reader :ucs_vlan_config, :changes
     
     def initialize opts
       fail unless opts.is_a? Hash
@@ -16,34 +17,107 @@ module Bunsen
       @connection
     end
     
+    def disconnect
+      @connection.logout
+    end
+    
+    def parse_config config
+      parsed = {}
+      config.each { |type,config|
+        case type
+        when /^vlans$/
+          @config_class = "fabricVlan"
+          parsed = parse_vlans config
+        end
+      }
+      parsed
+    end
+    
+    def parse_vlans config
+      new_config = {}
+      config.each { |vlan,opts|
+        
+        # Build default values allowing certain values to be left
+        # out of config.
+        # OPTIMIZE make defaults configurable
+        vlan_split = vlan.to_s.split("-")
+        opts[:id] ||= vlan_split[0].to_i.to_s
+        opts[:name] ||= vlan
+        opts[:mcastPolicyName] ||= "default"
+        opts[:defaultNet] ||= "no"
+        opts[:dn] = "%s/net-%s" % [opts[:domaingroup],opts[:name]]
+          
+        # Remove uneeded keys for ucs vlan
+        opts.delete(:dvs)
+        opts.delete(:domaingroup)
+        
+        # Prepare new hash
+        dn = opts[:dn]
+        new_config[dn] ||= {}
+        opts.each { |key,value|
+          new_config[dn][key] = value 
+        }  
+      }
+      @ucs_vlan_config = new_config
+    end
+    
+    def check_dn hash
+      changes = {}
+      hash.each { |dn,conf|
+        @connection.in_dn = dn
+        @connection.resolve_dn
+        changes = changes.merge(find_changes({dn => conf}, @connection.out_dn))
+        
+      }
+      @changes = changes
+      
+    end
+    
     def find_changes in_dn, out_dn
       changes = {}
-      out_dn.each do |odn, ohash|
-       in_dn.each do |idn, ihash|
-         case odn
-         when idn
-           changes[idn] ||= {}
-           ohash.each do |okey,oval|
-             ihash.each do |ikey,ival|
-               case okey
-               when ikey
-                 changes[idn][:old] ||= {}
-                 changes[idn][:new] ||= {}
-                 if oval == ival
-                   puts "%s not changed" % ikey
-                 else
-                   puts "%s changed" % ikey
-                   changes[idn][:old][okey] = oval
-                   changes[idn][:new][ikey] = ival
-                   
+      if out_dn.empty?
+        in_dn.each do |idn, ihash|
+        changes[idn] ||= {}
+        changes[idn][:status] = "create"
+        end
+      else
+        out_dn.each do |odn, ohash|
+         in_dn.each do |idn, ihash|
+           case odn
+           when idn.to_s
+             changes[idn] ||= {}
+             ohash.each do |okey,oval|
+               ihash.each do |ikey,ival|
+                 case okey
+                 when ikey.to_s
+                   changes[idn][:old] ||= {}
+                   changes[idn][:new] ||= {}
+                   changes[idn][:status] ||= "none"
+                   if oval == ival
+                     puts "%s not changed" % ikey.to_s
+                   else
+                     puts "%s changed" % ikey
+                     changes[idn][:status] = "update"
+                     changes[idn][:old][okey] = oval
+                     changes[idn][:new][ikey] = ival
+                     
+                   end
                  end
                end
              end
            end
          end
        end
-     end
+      end
+      
+
      changes
+    end
+    
+    def send_config opts
+      @connection.in_dn = opts
+      @connection.in_class = @config_class
+      @connection.config_mo
     end
     
   end
--- lib/bunsen/honeydew.rb
+++ lib/bunsen/honeydew.rb
@@ -3,19 +3,26 @@ require 'hashie'
 require 'bunsen'
 
 module Bunsen
-  class Honeydew    
+  class Honeydew
+    attr_accessor :connected_apis, :config, :auth
     
     def initialize opts
       opts[:bunsen_home] ||= "/etc/bunsen"
-      #opts[:config] ||= "honeydew.yaml"
+      opts[:config] ||= "honeydew.yaml"
       opts[:auth] ||= "auth.yaml"
         
-      #@config = "%s/%s" % [opts[:bunsen_home], opts[:config]]
+      @config = "%s/%s" % [opts[:bunsen_home], opts[:config]]
       @auth = "%s/%s" % [opts[:bunsen_home], opts[:auth]]
         
       load_auth
-      #load_config
-      
+      load_config
+      load_apis
+      test
+    end
+    
+    def test
+      test = instance_variable_get("@"+@connected_apis[0])
+      puts test
     end
     
     def load_auth
@@ -38,14 +45,41 @@ module Bunsen
     end
     
     def load_apis
+      connected = []
       @auth.each { |api, auth_hash|
         api_resolved = Bunsen::API.create api => auth_hash
-        puts api_resolved
         instance_variable_set("@" + api.to_s, api_resolved)
         instance_variable_get("@" + api.to_s)
         self.class.send(:attr_accessor, api)
+        connected << api.to_s
         
       }
+      @connected_apis = connected
+    end
+    
+    def config_ucs
+      @connected_apis.each do |api|
+        case api
+        when /^ucs$/
+          ucs = instance_variable_get("@" + api)
+          ucs.parse_config @config
+          ucs.check_dn ucs.ucs_vlan_config
+          ucs.changes.each do |dn,hash|
+            case hash[:status]
+            when /^create$/, /^update$/
+              puts "%s has changes" % dn
+              ucs.ucs_vlan_config.each do |conf_dn,conf_opts|
+                case dn
+                when conf_dn
+                  ucs.send_config conf_dn => conf_opts
+                end
+              end
+            when /^none$/
+              puts "no changes to make"
+            end
+          end
+        end
+      end
     end
     
     
