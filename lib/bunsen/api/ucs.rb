77ce97036282358e671df51b7e278c1efd3d8fba
Patrick Pimentel Jr <existsec@gmail.com>
2015-11-06 15:52:22 -0800

    lint: removing unused variables

--- lib/bunsen/api/ucs.rb
+++ lib/bunsen/api/ucs.rb
@@ -4,7 +4,7 @@ module Bunsen
   class UCS < Bunsen::API
     attr_accessor :connection
     attr_reader :vlan_config, :changes
-    
+
     def initialize opts
       fail unless opts.is_a? Hash
       fail unless opts[:user].is_a? String
@@ -12,19 +12,18 @@ module Bunsen
       fail unless opts[:host].is_a? String
       connect opts
     end
-    
+
     def connect opts
       @connection = Ucsimc::IMC.connect opts
       @connection
     end
-    
+
     def disconnect
       puts "Closing UCS Connection"
       @connection.logout
     end
-    
+
     def dew_config config
-      parsed = {}
       config.each { |type,content|
         case type
         when /^vnic_templates$/
@@ -40,11 +39,11 @@ module Bunsen
           @config_class = "vnicEtherIf"
           provision @assoc_vnic
           handle_changes @assoc_vnic
-           
+
         end
       }
     end
-    
+
     def handle_changes config
       @changes.each { |dn,hash|
         case hash[:status]
@@ -61,7 +60,7 @@ module Bunsen
         end
         }
     end
-    
+
     def parse type, content, config
       new_config = {}
       @assoc_vnic = {}
@@ -80,17 +79,17 @@ module Bunsen
               build_config = config_copy
             end
             vlan_split = item.to_s.split("-")
-            
+
             build_config[:id] ||= vlan_split[1].to_i.to_s
             build_config[:name] ||= item.to_s
             build_config[:dn] = "%s/net-%s" % [build_config[:domaingroup],build_config[:name]]
             valid_keys = [:id,:name,:mcastPolicyName,:defaultNet,:dn]
             vlan_vnic = vlan_associate_vnic(build_config, config)
             @assoc_vnic = @assoc_vnic.merge(vlan_vnic)
-              
+
             dn = build_config[:dn]
             new_config[dn] ||= {}
-  
+
             build_config.each { |key,value|
               case key
               when *valid_keys
@@ -116,10 +115,10 @@ module Bunsen
               end
               vnic_temp[:identPoolName] = "%s-%s" % [vnic_temp[:identPoolName], vnic_temp[:switchId].split("-")[0]]
               valid_keys = [:name,:dn,:mtu,:switchId,:nwCtrlPolicyName,:identPoolName,:pinToGroupName,:policyLevel,:policyOwner,:qosPolicyName,:statsPolicyName,:target,:templType]
-                                      
+
               dn = vnic_temp[:dn]
               new_config[dn] ||= {}
-                                    
+
               vnic_temp.each { |key,value|
                 case key
                 when *valid_keys
@@ -138,10 +137,10 @@ module Bunsen
                 vnic_temp[:switchId] = fabric_id
                 vnic_temp[:identPoolName] = "%s-%s" % [vnic_temp[:identPoolName], fabric_id]
                 valid_keys = [:name,:dn,:mtu,:switchId,:nwCtrlPolicyName,:identPoolName,:pinToGroupName,:policyLevel,:policyOwner,:qosPolicyName,:statsPolicyName,:target,:templType]
-                          
+
                 dn = vnic_temp[:dn]
                 new_config[dn] ||= {}
-                      
+
                 vnic_temp.each { |key,value|
                   case key
                   when *valid_keys
@@ -150,51 +149,51 @@ module Bunsen
                 }
               }
             end
-            
-            
-            
+
+
+
           end
         end
 
       }
       @ucs_config = new_config
     end
-    
+
     def provision hash
       changes = {}
       hash.each { |dn,conf|
         @connection.in_dn = dn
         @connection.resolve_dn
         changes = changes.merge(find_changes({dn => conf}, @connection.out_dn))
-        
+
       }
       @changes = changes
-      
+
     end
-    
+
     def find_changes in_dn, out_dn
       changes = {}
-        
+
       # catch an empty out_dn first so it gets processed
       # as a create before trying to be compared
       # an empty hash here indicates the dn in the config
       # doesn't resolve to anything in the ucs, thus it needs
       # to be created
-        
+
       if out_dn.empty?
         in_dn.each { |idn, ihash|
           changes[idn] ||= {}
           changes[idn][:status] = "create"
         }
       else
-        
+
         # compare the out_dn and in_dn hashes
         # out_dn is the full value returned from ucs
         # in_dn is the shortened to the required readwrite
         # settings, so many won't match but important ones will
         # when keys match, compare  the values and store
         # for reporting purposes
-        
+
         out_dn.each { |odn, ohash|
          in_dn.each { |idn, ihash|
            case odn
@@ -214,7 +213,7 @@ module Bunsen
                      changes[idn][:status] = "update"
                      changes[idn][:old][okey] = oval
                      changes[idn][:new][ikey] = ival
-                     
+
                    end
                  end
                }
@@ -223,16 +222,15 @@ module Bunsen
          }
         }
       end
-      
+
 
      changes
     end
-    
 
-    
+
+
     def vlan_associate_vnic build_config, config
       final_config = {}
-      vnic_conf = {}
       vnic_tmpl = config[:vnic_templates].reject {|h| h =~ /^defaults$/}
 
       if build_config[:vnic_template].is_a? Array
@@ -253,7 +251,7 @@ module Bunsen
                   final_config = final_config.merge(build_vlan(build_config, fabric_id, vnic_temp))
                 }
               end
-            end 
+            end
           }
         }
       else
@@ -278,7 +276,7 @@ module Bunsen
       end
       final_config
     end
-    
+
     def build_vlan build_config, fabric_id, vnic_temp
       new_config = {}
       vnic_conf = {}
@@ -300,9 +298,9 @@ module Bunsen
           vnic_conf[:defaultNet] = 'no'
         end
       end
-      vnic_conf[:dn] = "%s/lan-conn-templ-%s-%s/if-%s" %[build_config[:vnic_org],vnic_temp,fabric_id.downcase, build_config[:name]]
+      vnic_conf[:dn] = "%s/lan-conn-templ-%s-%s/if-%s" % [build_config[:vnic_org],vnic_temp,fabric_id.downcase, build_config[:name]]
       vnic_conf[:name] = build_config[:name]
-                          
+
       valid_keys = [:dn,:defaultNet,:name]
       dn = vnic_conf[:dn]
       new_config[dn] ||= {}
@@ -314,7 +312,7 @@ module Bunsen
       }
       new_config
     end
-    
+
     def send_config opts
       puts "\n\nStarting #{@config_class} Task"
       opts.each { |dn,conf|
@@ -325,8 +323,9 @@ module Bunsen
       @connection.in_class = @config_class
       start = Time.now
       @connection.config_mo
+
       puts "\nSpent %.2f seconds creating %s %s." % [(Time.now - start), @config_class, opts.keys[0]]
     end
-    
+
   end
 end
